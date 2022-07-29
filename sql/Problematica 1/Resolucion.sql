ee7156a609f684a9630b368120df88e3b4f19bb9
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-07-28 21:33:55 -0300

    .

--- sql/Problematica 1/Resolucion.sql
+++ sql/Problematica 1/Resolucion.sql
@@ -4,7 +4,7 @@
 
 --Correcion "Cliente"
 --Estructuramos la tabla
-IF EXISTS (SELECT * FROM cliente)
+IF EXISTS cliente
 BEGIN
 	CREATE TABLE "correccion"(
 		"customer_id" INTEGER NOT NULL,
@@ -12,12 +12,12 @@ BEGIN
 		"customer_surname"	INTEGER,
 		"customer_DNI"	INTEGER NOT NULL,
 		"dob" TEXT,
-		"customer_type_id"	INTEGER NOT NULL,
-		"costumer_address_id"	INTEGER NOT NULL,
-		"branch_id"	INTEGER NOT NULL,
+		"customer_type_id"	INTEGER,
+		"costumer_address_id"	INTEGER,
+		"branch_id"	INTEGER,
 		PRIMARY KEY("customer_id" AUTOINCREMENT),
-		FOREIGN KEY("customer_type_id") REFERENCES "tipo_cliente"("customer_type_id") ON DELETE NO ACTION ON UPDATE NO ACTION,
-		FOREIGN KEY("customer_address_id") REFERENCES "direccion"("address_id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+		--FOREIGN KEY("customer_type_id") REFERENCES "tipo_cliente"("customer_type_id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+		--FOREIGN KEY("customer_address_id") REFERENCES "direccion"("address_id") ON DELETE NO ACTION ON UPDATE NO ACTION,
 		FOREIGN KEY("branch_id") REFERENCES "sucursal"("branch_id") ON DELETE NO ACTION ON UPDATE NO ACTION
 	);
 END
