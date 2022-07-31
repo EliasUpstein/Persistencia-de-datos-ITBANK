19ca987de10495ea4ec10d9ff4985545a02f1f2a
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-07-31 20:57:11 -0300

    planteo de la ejecucion completa para resolver la problematica 1 desde un .py

--- problematica1/resolucion.py
+++ problematica1/resolucion.py
@@ -1,40 +1,46 @@
 from importlib.resources import path
 import sqlite3
-
-connection = sqlite3.connect('./itbank.db')
-cursor = connection.cursor()
+import os
 
 
-A_path = ("sql\reestructura_bd_1.sql")
-with open(A_path, 'r') as myfile:
-    data = myfile.read()
+def exSql(path):
+    file = open(path, 'r')
+    data = file.read()
     cursor.execute(data)
 
-
-B_path = ("sql\generar_datos_1.sql")
-with open(B_path, 'r') as myfile:
-    data = myfile.read()
-    cursor.execute(data)
 
 
 #La siguiente ejecucion permite vincular los datos de tipo de cliente de cada cliente en la tabla cuenta, dado que un cliente classic solo puede tener una caja en pesos, a diferencia del cliente classic que puede tener caja en dolares
-cursor.execute('SELECT * FROM cuenta')
-cuenta = cursor.fetchall()
+def tipoClienteACuenta(cuenta, cursor):
+    indice = 0
+    for row in cuenta:
+        sql = f''' 
+                UPDATE cuenta
+                SET account_type = (SELECT clie.customer_type_id
+                FROM cuenta acco INNER JOIN  cliente clie
+                ON acco.customer_id = clie.customer_id
+                LIMIT 1 OFFSET {indice})
+                WHERE account_id in( SELECT account_id FROM cuenta LIMIT 1 OFFSET {indice})
+                '''
+        cursor.execute(sql)
+        indice = indice + 1
+    #print(cuenta)
 
-indice = 0
-for row in cuenta:
-    sql = f''' 
-            UPDATE cuenta
-            SET account_type = (SELECT clie.customer_type_id
-            FROM cuenta acco INNER JOIN  cliente clie
-            ON acco.customer_id = clie.customer_id
-            LIMIT 1 OFFSET {indice})
-            WHERE account_id in( SELECT account_id FROM cuenta LIMIT 1 OFFSET {indice})
-            '''
-    cursor.execute(sql)
-    indice = indice + 1
+if __name__ == "__main__":
+    connection = sqlite3.connect('./itbank.db')
+    cursor = connection.cursor()
 
-#print(cuenta)
+    #A_path = os.path.join("sql", "1_reestructura_bd_A.sql")
+    A_path = "problematica1/sql/1_reestructura_bd_A.sql"
+    exSql(A_path)
 
-connection.commit()
-connection.close()
+    #B_path = os.path.join("sql", "2_generar_datos_A.sql")
+    B_path = "problematica1/sql/2_generar_datos_A.sql"
+    exSql(B_path)
+
+    cursor.execute('SELECT * FROM cuenta')
+    cuenta = cursor.fetchall()
+    #Para poder ejecutar esta funcion es necesario haber ejecutado los primeros 2 sql.
+    tipoClienteACuenta(cuenta, cursor)
+    connection.commit()
+    connection.close()
