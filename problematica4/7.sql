1706aa0bbf7588c9a2e607c79f1398b034a27359
Elias Upstein <eliasupstein@gmail.com>
2022-08-02 01:36:41 -0300

    Corrección item 7

--- problematica4/7.sql
+++ problematica4/7.sql
@@ -4,13 +4,24 @@
 --Registrar el movimiento en la tabla movimientos
 --En caso de no poder realizar la operación de forma completa, realizar un ROLLBACK
 
+--Creación de la tabla
 CREATE TABLE movimientos (
     transaction_id INT NOT NULL PRIMARY KEY,
     account_id INT NOT NULL,
     operation_tipe TEXT NOT NULL,
     amount INT NOT NULL,
     changed_at TEXT NOT NULL);
-BEGIN UPDATE cuenta
+
+--Transferencia
+UPDATE cuenta
 SET balance = balance - 1000
 WHERE account_id = 200;
-END;
+
+UPDATE cuenta
+SET balance = balance + 1000
+WHERE account_id = 400;
+
+--Registro de la transferencia
+INSERT INTO movimientos(account_id,operation_tipe,amount,changed_at) 
+VALUES(200,'-',1000,datetime('now'))
+VALUES(400,'+',1000,datetime('now'));
