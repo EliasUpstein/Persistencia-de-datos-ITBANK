4769a264aafd8a28aff722cdeedb4a8185267ea5
Elias Upstein <eliasupstein@gmail.com>
2022-08-02 00:54:56 -0300

    Resolución item 3

--- problematica4/3.sql
+++ problematica4/3.sql
@@ -1,1 +1,6 @@
 --Obtener la cantidad de tarjetas de crédito por tipo por sucursal
+
+SELECT branch_name, count(type_card) AS credito_count FROM cliente 
+INNER JOIN sucursal ON cliente.branch_id = sucursal.branch_id
+INNER JOIN tarjeta ON cliente.customer_id = tarjeta.customer_id
+GROUP BY branch_name HAVING type_card = "Crédito";
