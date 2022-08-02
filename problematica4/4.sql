cd40503f49dcb16cb44861be447f007c45dd1292
Elias Upstein <eliasupstein@gmail.com>
2022-08-02 00:34:23 -0300

    Corrección 1 y 4

--- problematica4/4.sql
+++ problematica4/4.sql
@@ -1,1 +1,6 @@
 --Obtener el promedio de créditos otorgado por sucursal
+
+SELECT branch_name, avg(loan_total) AS promedio FROM cliente 
+INNER JOIN sucursal ON cliente.branch_id = sucursal.branch_id
+INNER JOIN prestamo ON cliente.customer_id = prestamo.customer_id
+GROUP BY branch_name;
