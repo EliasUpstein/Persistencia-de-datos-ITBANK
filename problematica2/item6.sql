a7b5b1b5760b8fc88a8be9d8ee8688830a87c639
Elias Upstein <eliasupstein@gmail.com>
2022-08-01 22:55:45 -0300

    Correcciones, mejoras y documentación

--- problematica2/item6.sql
+++ problematica2/item6.sql
@@ -1,4 +1,4 @@
 --Eliminar el registro correspondiente a “Noel David” realizando la selección por el nombre y apellido
 
 DELETE FROM cliente
-WHERE customer_name LIKE 'David' AND customer_surname LIKE 'Noel';
+WHERE customer_name = 'Noel' AND customer_surname = 'David';
